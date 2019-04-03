ff9478d58fb4e96f2e6a4c91b44d04d9a080321e
Liwenhua <lwh@tedu.cn>
2019-04-03 21:58:45 +0800

    pool

--- pool.js
+++ pool.js
@@ -2,11 +2,11 @@
 //引入mysql模块
 const mysql=require('mysql');
 var pool=mysql.createPool({
-	host:'127.0.0.1',
-	port:'3306',
-	user:'root',
-	password:'',
-	database:'eatery',  //使用的数据库
+	host: process.env.MYSQL_HOST,
+	port: process.env.MYSQL_PORT,
+	user: process.env.ACCESSKEY,
+	password: process.env.SECRETKEY,
+	database: 'app_' + process.env.APPNAME,   //使用的数据库
 	connectionLimit:20  //设置连接池的数量
 });
 
